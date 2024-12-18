85ff868b4d97e7d39acd7f92bd3ac761f456782b
MD Sirajul Islam <153196838+SHOJIB-80@users.noreply.github.com>
2024-12-18 23:21:16 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
         int x; // a variable for x
 
         x=124; // giving x a value
-        long C=30000000000000000L; // use L for long
+        long C=3000000000000000000000000L; // use L for long
 
         System.out.println(" the value of x is " +x);
         System.out.println("the value of C is "+C);
