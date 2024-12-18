a106c5997939678e8ccf4dd1fb8fd8e25ac78bfa
MD Sirajul Islam <153196838+SHOJIB-80@users.noreply.github.com>
2024-12-18 23:19:05 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int x; // a variable for x
+
+        x=124; // giving x a value
+        long C=30000000000000000L; // use L for long
+
+        System.out.println(" the value of x is " +x);
+        System.out.println("the value of C is "+C);
+    }
+}
